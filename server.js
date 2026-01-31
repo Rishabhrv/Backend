86d0909fe0fed1cf3b35920b36a349de25b8048f
Priyanshu <priyanshusamanta101@gmail.com>
2026-01-31 10:30:11 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 /* ROUTES */
 app.use("/api/auth", require("./routes/auth"));
